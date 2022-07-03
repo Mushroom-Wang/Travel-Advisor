7367dfe58a492ee488c89ea4c3a2fb1d8c3cad3a
Quan Wang <quanwang452@gmail.com>
2022-07-03 18:11:29 -0400

    create post model to add post and store it in mongodb

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 let express = require('express');
+const { Post } = require('./models/posts');
 let app = express();
+let mongoose = require('mongoose')
+let post = require('./models/posts').Post;
+
+mongoose.connect('mongodb://localhost/travels', { useNewUrlParser: true });
+
+let post1 = new Post({
+    id: 2,
+    title: 'Statue of Liberty',
+    date: new Date(),
+    description: 'some description',
+    text: 'some text',
+    country: 'USA',
+    imageURL: '/images/2.jpg'
+});
+
+post1.save()
 
 app.use(express.static('public'));
 
